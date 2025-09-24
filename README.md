3588f0f03b333e27225fa8642aede80627259047
alvarado0925 <alvarado0925@gmail.com>
2025-09-24 15:25:08 -0500

    feat: Agregar documentación Swagger y mejorar anotaciones en PizzaController

---
+++ README.md
@@ -0,0 +1,3 @@
+# PizzaYa API
+
+You can access the API documentation [Swagger Docs](http://localhost:8082/movieverse/api/swagger-ui/index.html).
--- src/main/java/com/afperdomo2/pizzaya/web/controller/PizzaController.java
+++ src/main/java/com/afperdomo2/pizzaya/web/controller/PizzaController.java
@@ -11,8 +11,15 @@ import org.springframework.web.bind.annotation.RestController;
 import com.afperdomo2.pizzaya.persistence.entity.PizzaEntity;
 import com.afperdomo2.pizzaya.service.PizzaService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 @RestController
 @RequestMapping("/pizzas")
+@Tag(name = "Pizzas", description = "Operaciones relacionadas con las pizzas")
 public class PizzaController {
     private final PizzaService pizzaService;
 
@@ -21,12 +28,18 @@ public class PizzaController {
     }
 
     @GetMapping
+    @Operation(summary = "Obtener todas las pizzas", description = "Devuelve una lista de todas las pizzas")
+    @ApiResponse(responseCode = "200", description = "Lista de pizzas obtenida exitosamente")
     public ResponseEntity<List<PizzaEntity>> findAll() {
         return ResponseEntity.ok(this.pizzaService.findAll());
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<PizzaEntity> findById(@PathVariable Long id) {
+    @Operation(summary = "Obtener una pizza por ID", description = "Devuelve una pizza específica")
+    @ApiResponse(responseCode = "200", description = "Pizza encontrada")
+    @ApiResponse(responseCode = "404", description = "Pizza no encontrada", content = @Content())
+    public ResponseEntity<PizzaEntity> findById(
+            @Parameter(description = "ID de la pizza", example = "1") @PathVariable Long id) {
         PizzaEntity pizza = this.pizzaService.findById(id);
         if (pizza == null) {
             return ResponseEntity.notFound().build();
