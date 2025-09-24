a1af3b399d3fc2a55a7e8017ef75e3b23c81d06d
alvarado0925 <alvarado0925@gmail.com>
2025-09-24 10:34:57 -0500

    First commit

---
+++ src/main/java/com/afperdomo2/pizzaya/PizzayaApplication.java
@@ -0,0 +1,13 @@
+package com.afperdomo2.pizzaya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzayaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzayaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/afperdomo2/pizzaya/PizzayaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.afperdomo2.pizzaya;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzayaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
