5bd65176bf55f671dde4366e3fe01c7aa84eda84
alvarado0925 <alvarado0925@gmail.com>
2025-10-06 11:08:00 -0500

    feat: Actualizar la configuración de seguridad para permitir solicitudes GET de pizzas y denegar solicitudes PATCH

--- src/main/java/com/afperdomo2/pizzaya/web/config/SecurityConfig.java
+++ src/main/java/com/afperdomo2/pizzaya/web/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.afperdomo2.pizzaya.web.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -15,6 +16,9 @@ public class SecurityConfig {
                 .cors(Customizer.withDefaults())
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(customizeRequests -> {
+                    customizeRequests.requestMatchers(HttpMethod.GET, "/pizzas/**").permitAll();
+                    customizeRequests.requestMatchers(HttpMethod.PATCH).denyAll();
+
                     customizeRequests.anyRequest().authenticated();
                 })
                 .httpBasic(Customizer.withDefaults());
