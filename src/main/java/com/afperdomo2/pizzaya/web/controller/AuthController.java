4afb1cdece39e86472b444ca01c1b5e3477808e2
alvarado0925 <alvarado0925@gmail.com>
2025-10-06 23:01:44 -0500

    feat: Configurar OpenAPI para incluir esquema de seguridad JWT y permitir acceso público al endpoint de inicio de sesión

--- src/main/java/com/afperdomo2/pizzaya/web/controller/AuthController.java
+++ src/main/java/com/afperdomo2/pizzaya/web/controller/AuthController.java
@@ -3,6 +3,7 @@ package com.afperdomo2.pizzaya.web.controller;
 import com.afperdomo2.pizzaya.service.dto.SignInDto;
 import com.afperdomo2.pizzaya.web.config.JwtUtil;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirements;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
@@ -30,11 +31,10 @@ public class AuthController {
 
     @PostMapping("/sign-in")
     @Operation(summary = "Iniciar sesión", description = "Permite a un usuario iniciar sesión en el sistema")
+    @SecurityRequirements() // Este endpoint NO requiere autenticación
     public ResponseEntity<Void> signIn(@RequestBody SignInDto signInDto) {
         var login = new UsernamePasswordAuthenticationToken(signInDto.getUsername(), signInDto.getPassword());
         Authentication authentication = authenticationManager.authenticate(login);
-
-
 
         System.out.println(authentication);
         System.out.println(authentication.isAuthenticated());
