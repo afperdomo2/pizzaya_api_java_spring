d47517ee2db91917805d0684dc5320409cbdff1a
alvarado0925 <alvarado0925@gmail.com>
2025-10-07 10:40:53 -0500

    feat: Agregar campos de auditoría para los creados y actualizados por el usuario en AuditableEntity

--- src/main/java/com/afperdomo2/pizzaya/persistence/audit/AuditableEntity.java
+++ src/main/java/com/afperdomo2/pizzaya/persistence/audit/AuditableEntity.java
@@ -2,7 +2,9 @@ package com.afperdomo2.pizzaya.persistence.audit;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.MappedSuperclass;
+import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
 
 import java.time.LocalDateTime;
@@ -16,4 +18,12 @@ public class AuditableEntity {
     @Column(name = "updated_at")
     @LastModifiedDate
     private LocalDateTime updatedAt;
+
+    @Column(name = "created_by", updatable = false)
+    @CreatedBy
+    private String createdBy;
+
+    @Column(name = "updated_by")
+    @LastModifiedBy
+    private String updatedBy;
 }
